753b533ee4588a5ffefb9ce5c0069651ff995b5f
Brie <brienne.mcnally@segment.com>
2020-10-06 16:54:20 -0600

    Update conditional

--- Segment-Nielsen-DCR/Classes/SEGNielsenDCRIntegration.m
+++ Segment-Nielsen-DCR/Classes/SEGNielsenDCRIntegration.m
@@ -7,7 +7,7 @@
 //
 
 #import "SEGNielsenDCRIntegration.h"
-#if defined(__has_include) && __has_include(<Analytics/Analytics.h>)
+#if defined(__has_include) && __has_include(<Analytics/SEGAnalytics.h>)
 #import <Analytics/SEGAnalyticsUtils.h>
 #else
 #import <Segment/SEGAnalyticsUtils.h>
