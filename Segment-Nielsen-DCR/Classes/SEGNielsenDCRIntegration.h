a12d341ecf0783ef197804da2e256809cb236f04
Brie McNally <31782219+briemcnally@users.noreply.github.com>
2020-10-07 12:46:03 -0600

    [STRATCONN-419] Update namespacing ios14 (#22)
    * Update namespacing ios14
    
    * Update conditional

--- Segment-Nielsen-DCR/Classes/SEGNielsenDCRIntegration.h
+++ Segment-Nielsen-DCR/Classes/SEGNielsenDCRIntegration.h
@@ -6,7 +6,11 @@
 //
 
 #import <Foundation/Foundation.h>
+#if defined(__has_include) && __has_include(<Analytics/SEGAnalytics.h>)
 #import <Analytics/SEGIntegration.h>
+#else
+#import <Segment/SEGIntegration.h>
+#endif
 #import <NielsenAppApi/NielsenAppApi.h>
 
 
